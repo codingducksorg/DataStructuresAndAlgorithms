7edc8e3dc5f92ad7ddc399df8582a1ddae08b8ac
codingducksorg <codingducks@gmail.com>
2017-06-08 02:07:03 +0530

    Create DecimaltoBinary.py

---
+++ BitManipulations/DecimaltoBinary.py
@@ -0,0 +1,20 @@
+# Hello World program in Python
+    
+print "Hello World!\n"
+def convertToBinary(num):
+  """Function to print binary number
+  for the input decimal"""
+  binary = 0
+  base = 1
+  while num > 0:
+    remainder = num % 2
+    binary = binary + remainder * base
+    num = num / 2
+    base = base * 10
+
+  print "The binary of the given number is ", binary, '.'
+
+# decimal number
+dec=1910
+
+convertToBinary(dec)
